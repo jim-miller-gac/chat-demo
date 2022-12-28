8ea8b2275d9c5583224d90be7f629636a3edaae7
jim-miller-gac <jim.miller.gac@gmail.com>
2022-12-27 19:45:49 -0600

    yet another update

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ io.on('connection', (socket) => {
          socket.emit('chat message', disconnectNotice);
          console.log( disconnectNotice);
          socket.disconnect();
-      }, 1 * 20 * 1000); // 5ky 60n 30n 10y 15y 25n 20
+      }, 1 * 15 * 1000); // 5ky 60n 30n 10y 15y 25n 20n
       
       io.emit('chat message', msg);
    });
